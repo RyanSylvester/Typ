27be4052b7937245a03d5139cd954ab436d8ca34
Ryan <ryansylvester21@gmail.com>
2022-05-07 20:12:27 -0400

    word preview deck integrated

--- src/components/Feeder/index.js
+++ src/components/Feeder/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
 export default function Index({
-    activeWord
+    activeWord,
+    onDeck
 }) {
+    
+    const onDeckDisplay = onDeck.map((word, i) =>
+        <div className={'onDeckWords'} style={{opacity:1-(0.1*i)}}>{word}</div>
+    )
     return (
         <div className={'feeder'}>
-            <div>{activeWord}</div>
+            <div className={'activeWord'}>{activeWord}</div>
+            <div className={'onDeck'}>
+                {onDeckDisplay}
+            </div>
         </div>
     )
 }
